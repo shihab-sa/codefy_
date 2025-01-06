3449d480c82061f3f5edf142c66343b381ebf37c
shihabahmed <shihabbee@gmail.com>
2025-01-06 10:09:02 +0600

    arr 1,2

---
+++ Array/Very_Easy/02.js
@@ -0,0 +1,22 @@
+// Pair Management
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+// makePair(1, 2) ➞ [1, 2]
+
+// makePair(51, 21) ➞ [51, 21]
+
+// makePair(512124, 215) ➞ [512124, 215]
+// Notes
+// N/A
+
+function makePair(arr1, arr2) {
+  let newArrPair = [arr1, arr2];
+  console.log(newArrPair);
+}
+
+makePair(1, 2); //➞ [1, 2]
+
+makePair(51, 21); //➞ [51, 21]
+
+makePair(512124, 215); //➞ [512124, 215]
